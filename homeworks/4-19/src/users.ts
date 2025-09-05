114b3b1523f3030f610f030544b06ec7db53715f
Kirill Belovodenko <Delirium4Dude@gmail.com>
2025-09-05 20:42:32 +0300

    done

---
+++ homeworks/4-19/src/users.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+const urlStr:string = `https://dummyjson.com/users`;
+
+enum gender {
+  male = 'male',
+  female = 'female'
+}
+
+type User = {
+  id: number,
+  firstName: string,
+  lastName: string,
+  maidenName: string,
+  age: number,
+  gender: keyof typeof gender
+}
+
+async function getUsers():Promise<User[] | Error> {
+  const resp = await axios.get<User[]>(urlStr);
+  if (resp.status !== 200) {
+    throw new Error('Ошибка при получении данных');
+  }
+  return resp.data.map(user => ({
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    maidenName: user.maidenName,
+    age: user.age,
+    gender: user.gender
+  }));
+}
+
+getUsers()
+  .then(users => {
+    console.log(users);
+  }).catch(err => {
+    console.error(err);
+  });
